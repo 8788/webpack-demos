2569ffe2f33eb09676538bd89346a6b488eddfc4
8788 <2802513+8788@users.noreply.github.com>
2018-12-10 21:17:09 +0800

    add dev-server

---
+++ dev-server/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    host: '127.0.0.1',
+    port: 81,
+    contentBase: path.join(__dirname, 'dist'),
+    open: true,
+    hot: true,
+    disableHostCheck: true,
+    proxy: {
+      // http://127.0.0.1:81/v2/5c0e62192e00005400043e63
+      '/v2': {
+        target: 'http://www.mocky.io/',
+        changeOrigin: true
+      }
+    },
+    before (app) {
+      app.get('/api', function(req, res) {
+        res.json({ code: 200, msg: 'mock test' })
+      })
+    }
+  }
+}
