4a3473591bb56ca88e952c23c2a080486bc60bbf
8788 <2802513+8788@users.noreply.github.com>
2018-12-18 18:45:53 +0800

    add image-base64

---
+++ image-base64/webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 4 * 1024, // inline files smaller than 4k
+              name: '[name].[hash:6].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              limit: 4 * 1024, // inline files smaller than 4k
+              name: '[name].[hash:6].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+    new OptimizeCSSAssetsPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    host: '127.0.0.1',
+    port: 8081,
+    contentBase: path.join(__dirname, 'dist'),
+    open: true,
+    hot: true,
+    disableHostCheck: true
+  }
+}
